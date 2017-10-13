8768aaf9b4106a471b08f95b6a0549b31baa240d
adragonrc <adragonrc@gmail.com>
2017-10-12 23:18:49 -0500

    first commit

---
+++ src/primeraapp/PrimeraApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeraapp;
+
+/**
+ *
+ * @author adragonrc
+ */
+public class PrimeraApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona ob1 = new Persona("Allex", 18, 72017649);
+        System.out.println(ob1.toString());
+        
+        
+    }
+    
+}
